808af98e901a39eb65ed8503197c15326f803ff7
FrontendDev-Basheer <basheerboss788@gmail.com>
2024-05-01 10:23:05 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link } from 'react-scroll';
 
 function App() {
+  const navItems = [
+    {
+      id:1,
+      title : 'About'
+    },
+    {
+      id:2,
+      title : 'Skills'
+    },
+    {
+      id:3,
+      title : 'Experience'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <h2>My Portfolio</h2>
+          <ul>
+            {
+              navItems.map(menu=>(
+                <li>
+                <Link to={menu.title} smooth={true} offset={-300} duration={500}>{menu.title}</Link>           
+                </li>  
+              ))
+            }
+          </ul>
+        </nav>
+          
       </header>
+      <main>
+        {
+        navItems.map(menu=>( 
+          <div className='content'>
+          <h1 className='content-header' id={menu.title}>{menu.title}</h1>
+        </div>
+        ))
+
+        }
+
+      </main>
     </div>
   );
 }
